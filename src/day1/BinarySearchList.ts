b39b7bbcec4f30c48e615818af4dfeb71916c71e
Phobia <filipe_msr@hotmail.com>
2023-08-28 21:09:39 -0300

    Binary Search List

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let start = 0;
+  let end = haystack.length;
 
+  do {
+    const mid = Math.floor(start + (end - start) / 2);
+    const value = haystack[mid];
+
+    if (value === needle) return true;
+
+    if (value > needle) {
+      end = mid;
+    } else {
+      start = mid + 1;
+    }
+
+  } while (start < end)
+
+  return false;
 }
